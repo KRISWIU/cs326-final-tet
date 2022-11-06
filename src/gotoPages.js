570c80a5c2e2fb6a471bb32c3245931d63be8b39
iiisso <zhenduowang@umass.edu>
2022-11-06 11:54:18 -0500

    save some change about js

---
+++ src/gotoPages.js
@@ -0,0 +1,9 @@
+function gotoMainPage(){
+    window.location.rel="external nofollow";
+    window.location.href="main.html";
+}
+
+function gotoAddWork(){
+    window.location.rel="external nofollow";
+    window.location.href = "addPage.heml";
+}
---
+++ src/main.js
@@ -0,0 +1,6 @@
+import 'gotoPages.js';
+//nav bar js code
+
+document.getElementById("siteName").addEventListener("click", gotoMainPage());
+
+document.getElementById('addWork').addEventListener('click', gotoAddWork());
