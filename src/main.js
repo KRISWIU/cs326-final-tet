15b7ba5de6dda595b6a8861e02ba766a565cab85
iiisso <zhenduowang@umass.edu>
2022-11-09 23:25:46 -0500

    added some event lisetener

--- src/main.js
+++ src/main.js
@@ -1,8 +1,4 @@
 //import * as addpage from 'gotoPages.js';
-//nav bar js code
-
-// document.getElementById("siteName").addEventListener("click", gotoMainPage());
-// document.getElementById('addWork').addEventListener('click', gotoAddWork());
 
 const baseURL = "https://the-artchive.herokuapp.com";
 const localBaseURL = "http://localhost:8000";
@@ -31,4 +27,34 @@ window.onload = () => {
             console.log("The response object is: " + JSON.stringify(returnObj)); })();  
     });
     // End of event listener
+
+    document.getElementById("loginProfileButton").addEventListener("click", async ()=>{
+        const loginBut = document.getElementById("loginProfileButton");
+        const userName = loginBut.value;
+        const URL = localBaseURL + "/users/:" + userName;
+
+        (async () => {
+            const returnObj = await makeRequest(URL, "POST"); 
+            console.log("The response object is: " + JSON.stringify(returnObj)); })();  
+    });
+
+    document.getElementById("filterMenu").addEventListener("click", async ()=>{
+        const tags = document.getElementById("tags");
+        const creators = document.getElementById("creators");
+        let URL = '';
+        if (tags.value!==null && creators.value!==null){
+            URL = localBaseURL + "/tags/creators/:" + creators.value;
+        }else if(tags.value!==null){
+            URL = localBaseURL + "/tags/:" + tags.value;
+        }else if(creators.value!==null){
+            URL = localBaseURL + "/tags/creators/:" + creators.value;
+        }
+
+        (async () => {
+            const returnObj = await makeRequest(URL, "POST"); 
+            console.log("The response object is: " + JSON.stringify(returnObj)); })();  
+    });
+
+    document.getElementById("siteName").addEventListener("click", gotoMainPage());
+    document.getElementById('addWork').addEventListener('click', gotoAddWork());
 };
