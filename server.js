43824188471ecfcd6f2d7e95b3cc767e76362c99
Sir. Vival <jacobg103@charter.net>
2022-11-09 19:18:44 -0500

    Managed to get very basic commands working.

--- server.js
+++ server.js
@@ -2,12 +2,42 @@ const { response } = require('express');
 const express = require('express');
 const app = express();
 const port = 8000;
+const router = express.Router();
+
+// Serve basic pages
+app.use(express.static('src'))
 
 
-// app.use()
+// Implement the commands from the documentation and add statements for all of the possible routes. 
+// Functions don't actually have to do much right now
 
-app.get('/', function(req, res) {
-    app.use(express.static('src'));
+
+//  ###  GET  ###  \\
+
+app.get("/artworks/:artwork", (req, res) => {
+    res.write("artwork retrieval called.");
+    res.end();
 });
+
+app.get("/artworks/search", (req, res) => {
+    res.write("artwork search called.");
+});
+
+// The rest go here
+
+//  ###  POST  ###  \\
+
+app.post("/artworks", (req, res) => {
+    res.write("artwork creation called.");
+});
+
+
+//  ###  PUT  ###  \\
+
+
+//  ###  DELETE  ###  \\
+
+
+
 
 app.listen(port);
