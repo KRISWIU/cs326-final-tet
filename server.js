e8c71e2f715b74d3e4a5352effcf07aeceab38d7
Sir. Vival <jacobg103@charter.net>
2022-11-14 18:15:36 -0500

    changed process.env.port to have uppercase PORT

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app = express();
 console.log("Server successfully started.");
-const port = process.env.port || 5000;
+const port = process.env.PORT || 5000;
 console.log("Selected port number is: " + port);
 // const router = express.Router();
 
